5c816bab1f218cc81d72e77feae8cbe4f7b859a3
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2025-04-15 15:10:42 +0800

    update nav design

--- src/components/dashboardPage.tsx
+++ src/components/dashboardPage.tsx
@@ -5,19 +5,44 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { BillList } from "~/app/_components/billList";
 import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 
 const authClient = createAuthClient();
 
-export function DashboardPage() {
+function UserNav() {
   const router = useRouter();
+  const { data: session } = authClient.useSession();
   const [isSigningOut, setIsSigningOut] = useState(false);
 
   return (
-    <div className="flex flex-col gap-4">
-      <nav className="flex min-h-14 items-center border-b px-6 shadow">
-        <Button
-          variant="link"
-          className="ml-auto"
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"} className="h-8 w-8 rounded-full">
+          <Avatar>
+            <AvatarImage src={session?.user?.image ?? undefined} />
+            <AvatarFallback>
+              {session?.user?.name
+                .toUpperCase()
+                .split(" ")
+                .map((i) => i.slice(0, 1))
+                .slice(0, 2)
+                .join("") ?? "..."}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align={"end"} className="w-[200px]">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
           onClick={async () => {
             setIsSigningOut(true);
             await authClient.signOut({
@@ -30,7 +55,17 @@ export function DashboardPage() {
           disabled={isSigningOut}
         >
           Sign out
-        </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function DashboardPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <nav className="flex h-14 items-center justify-end border-b px-6">
+        <UserNav />
       </nav>
       <div className="flex-grow p-6">
         <BillList />
