cd2cc9842d70d6d715123336284e52a0a9e4ba9d
Jed <54795428+jedymatt@users.noreply.github.com>
2026-06-20 13:26:57 +0800

    Add oAuthProxy and update allowed hosts

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,7 +1,7 @@
 import type { BetterAuthOptions } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { nextCookies } from "better-auth/next-js";
-import { anonymous } from "better-auth/plugins";
+import { anonymous, oAuthProxy } from "better-auth/plugins";
 import { env } from "~/env";
 import { db } from "~/server/db";
 
@@ -10,6 +10,7 @@ export const authConfig = {
   baseURL: {
     allowedHosts: [
       "localhost:3000",
+      "www.remindmebills.com",
       "remindmebills.com",
       "remindmebills.vercel.app",
       "remindmebills-*-jedymatt-personal.vercel.app",
@@ -20,11 +21,15 @@ export const authConfig = {
     google: {
       clientId: env.AUTH_GOOGLE_ID,
       clientSecret: env.AUTH_GOOGLE_SECRET,
+      redirectURI: "https//www.remindmebills.com",
     },
   },
   plugins: [
     nextCookies(),
     anonymous(),
+    oAuthProxy(
+      productionURL: "https://www.remindmebills.com"
+    ),
   ],
   account: {
     modelName: "accounts",
